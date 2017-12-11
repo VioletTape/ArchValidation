22ace52071bafe15620d6744ac6541570777a788
Andrei Gordienkov <andrei.gordienkov@volvo.com>
2017-12-11 13:12:42 +0100

    move ref checks for interfaces

---
+++ DomainB/Aspects/NoExplicitInstantiaionPolicy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using PostSharp.Constraints;
+using PostSharp.Extensibility;
+using PostSharp.Reflection;
+
+namespace DomainB.Aspects {
+    [MulticastAttributeUsage(MulticastTargets.Interface, Inheritance = MulticastInheritance.Multicast)]
+    public class NoExplicitInstantiaionPolicy : ReferentialConstraint
+    {
+        public override void ValidateCode(object target, Assembly assembly)
+        {
+            var targetType = (Type)target;
+            var usages = ReflectionSearch.GetDerivedTypes(targetType);
+
+            foreach (var usage in usages)
+            {
+                var constructorInfos = usage.DerivedType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+
+                foreach (var ctr in constructorInfos)
+                {
+                    var illeagalUsage = ReflectionSearch.GetMethodsUsingDeclaration(ctr);
+
+                    foreach (var usageRefs in illeagalUsage)
+                        Message.Write(usageRefs.UsingMethod, SeverityType.Error, "re001", "There should be no explicit instantiations for {0}", usageRefs.UsingMethod.DeclaringType);
+                }
+            }
+
+            base.ValidateCode(target, assembly);
+        }
+    }
+}
---
+++ DomainB/ServicesB/IMyService.cs
@@ -0,0 +1,27 @@
+﻿using DomainB.Aspects;
+
+namespace DomainB.ServicesB {
+    public class MyService : IMyService
+    {
+        public void Boo() { }
+    }
+
+    [NoExplicitInstantiaionPolicy]
+    public interface IMyService
+    {
+        void Boo();
+    }
+
+    public class MySuperService
+    {
+        public void Foo(MyService myService){
+            //            var service = new MyService(); // uncomment for error
+            myService.Boo();
+        }
+
+        public void Foo2(MyService myService){
+            //            var service = new MyService(); // uncomment for error
+            myService.Boo();
+        }
+    }
+}
